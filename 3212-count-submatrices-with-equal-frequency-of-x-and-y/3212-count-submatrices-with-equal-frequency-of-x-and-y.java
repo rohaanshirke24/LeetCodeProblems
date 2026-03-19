72ebbc68df434c56bb6c3211cc082ee6d9f640e6
Rohaan Shirke <57215607+rohaanshirke24@users.noreply.github.com>
2026-03-19 08:46:08 +0530

    Time: 24 ms (69.23%), Space: 249.2 MB (84.62%) - LeetHub

---
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] x = new int[m + 1][n + 1];
+        int[][] y = new int[m + 1][n + 1];
+
+        int ans = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                x[i][j] = x[i - 1][j] + x[i][j - 1] - x[i - 1][j - 1]
+                        + (grid[i - 1][j - 1] == 'X' ? 1 : 0);
+
+                y[i][j] = y[i - 1][j] + y[i][j - 1] - y[i - 1][j - 1]
+                        + (grid[i - 1][j - 1] == 'Y' ? 1 : 0);
+
+                if (x[i][j] > 0 && x[i][j] == y[i][j]) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
