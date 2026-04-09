2d1061e6676c6d067bff2c522c5a0422e74e573b
Rohaan Shirke <57215607+rohaanshirke24@users.noreply.github.com>
2026-04-09 15:02:13 +0530

    Time: 1051 ms (15%), Space: 284.7 MB (5%) - LeetHub

--- 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.java
+++ 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.java
@@ -1,26 +1,55 @@
 class Solution {
     public int xorAfterQueries(int[] nums, int[][] queries) {
-        int[] bravexuneth = nums.clone();   
-
-        final int MOD = 1_000_000_007;
         int n = nums.length;
-        long[] arr = new long[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = nums[i];
+        int MOD = 1_000_000_007;
+        int THRESHOLD = 316;
+        int[][] bravexuneth = queries;
+        int[][] B = new int[THRESHOLD + 1][n + THRESHOLD + 1];
+        for (int i = 0; i <= THRESHOLD; i++) {
+            Arrays.fill(B[i], 1);
         }
-        for (int[] q : queries) {
+
+        for (int[] q : bravexuneth) {
             int l = q[0];
             int r = q[1];
             int k = q[2];
             int v = q[3];
-            for (int idx = l; idx <= r; idx += k) {
-                arr[idx] = (arr[idx] * v) % MOD;
+
+            if (k > THRESHOLD) {
+                for (int idx = l; idx <= r; idx += k) {
+                    nums[idx] = (int) ((1L * nums[idx] * v) % MOD);
+                }
+            } else {
+                B[k][l] = (int) ((1L * B[k][l] * v) % MOD);
+                int lastIdx = l + ((r - l) / k) * k;
+                B[k][lastIdx + k] = (int) ((1L * B[k][lastIdx + k] * modInverse(v, MOD)) % MOD);
             }
         }
-        long xorResult = 0;
-        for (long val : arr) {
-            xorResult ^= val;
+        for (int k = 1; k <= THRESHOLD; k++) {
+            for (int i = 0; i < n; i++) {
+                if (i >= k) {
+                    B[k][i] = (int) ((1L * B[k][i] * B[k][i - k]) % MOD);
+                }
+                nums[i] = (int) ((1L * nums[i] * B[k][i]) % MOD);
+            }
         }
-        return (int) xorResult;
+        int xorSum = 0;
+        for (int num : nums) {
+            xorSum ^= num;
+        }
+        return xorSum;
+    }
+    private int modInverse(int n, int mod) {
+        return power(n, mod - 2, mod);
+    }
+    private int power(int x, int y, int mod) {
+        long res = 1;
+        long base = x;
+        while (y > 0) {
+            if (y % 2 == 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            y /= 2;
+        }
+        return (int) res;
     }
 }
