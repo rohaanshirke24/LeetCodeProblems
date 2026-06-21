1d7e40dec2c68072f97cb0da90cd80f84e643f3e
Rohaan Shirke <57215607+rohaanshirke24@users.noreply.github.com>
2026-06-21 21:13:01 +0530

    Time: 31 ms (54.08%), Space: 48.4 MB (67.27%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int type : candyType) {
+            set.add(type);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
