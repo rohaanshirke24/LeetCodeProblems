b832436219fd96fa495de9f64f5c3c8eb6409a75
Rohaan Shirke <57215607+rohaanshirke24@users.noreply.github.com>
2026-07-02 21:32:40 +0530

    Time: 3 ms (88.38%), Space: 44.7 MB (71.43%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String sortString(String s) {
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (ans.length() < s.length()) {
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] > 0) {
+                    ans.append((char) (i + 'a'));
+                    freq[i]--;
+                }
+            }
+
+            for (int i = 25; i >= 0; i--) {
+                if (freq[i] > 0) {
+                    ans.append((char) (i + 'a'));
+                    freq[i]--;
+                }
+            }
+        }
+
+        return ans.toString();
+    }
+}
