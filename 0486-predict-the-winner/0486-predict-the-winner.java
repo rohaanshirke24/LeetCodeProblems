b69a72b0ba1f2515202477b5774d3fe08e85083d
Rohaan Shirke <57215607+rohaanshirke24@users.noreply.github.com>
2026-08-01 10:45:32 +0530

    Time: 1 ms (51.87%), Space: 42.9 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        
+        return dp[0][n - 1] >= 0;
+    }
+}
